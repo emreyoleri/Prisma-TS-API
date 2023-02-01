a6ecc67ac42a340c71697d798d7aade61e053671
Emre Yoleri <emreyoleri01@gmail.com>
2023-02-01 03:36:10 +0300

    Book Services and Router

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 
 import { authorRouter } from "./author/author.router";
+import { bookRouter } from "./book/book.router";
 
 dotenv.config();
 
@@ -16,7 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/authors", authorRouter);
+app.use("/api/books", bookRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server started on  http://localhost:${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
